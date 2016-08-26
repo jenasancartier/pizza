8e4d50a1b8b996ff01619a653e7430826688c9ba
Jena SanCartier <jenasancartier@gmail.com>
2016-08-26 07:58:33 -0700

    create pizza.prototype to calculate price of pizza options, complete prices for pizza sizes

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,17 @@ function Pizza(size, protein, veggies) {
   this.veggies = veggies;
 }
 
-// Pizza.prototype.calculatePrice = function() {
-//
-// }
+Pizza.prototype.calculatePrice = function() {
+  var pizzaPrice = 0;
+  if (this.size === "Small") {
+    pizzaPrice += 8;
+  } else if (this.size === "Medium") {
+    pizzaPrice += 12;
+  } else if (this.size === "Large") {
+    pizzaprice += 16;
+  }
+  return pizzaPrice;
+}
 
 //user interface logic:
 $(document).ready(function(){
@@ -32,9 +40,8 @@ $(document).ready(function(){
     $(".output").show();
     $("#showSize").text(selectSize);
     $("#showProtein").text(selectProtein);
-    $("#showVeggies").text(selectVeggies.join(" "));
-
-
+    $("#showVeggies").text(selectVeggies.join(", "));
+    $("#pizzaPrice").text(newPizza.calculatePrice());
 
 
   });
